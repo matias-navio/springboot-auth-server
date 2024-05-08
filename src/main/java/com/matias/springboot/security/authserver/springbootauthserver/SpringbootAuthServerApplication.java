cd86fb34aab6770d7524b417fe013a04f35014c0
Matias Navio <naviomatias2@gmail.com>
2024-05-08 09:13:47 -0300

    Initial commit

---
+++ src/main/java/com/matias/springboot/security/authserver/springbootauthserver/SpringbootAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.security.authserver.springbootauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/springboot/security/authserver/springbootauthserver/SpringbootAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.security.authserver.springbootauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
